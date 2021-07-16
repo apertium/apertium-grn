44321911ee4fdbb2ede25661417561d939d5d010
Daniel Swanson <popcorn.tomato.dude@gmail.com>
2021-07-16 11:58:59 -0500

    convert tests to apertium-regtest

--- tests/coverage/coverage.sh
+++
@@ -1,6 +0,0 @@
-total=`find . -name 'grn_human_rights.txt' | xargs cat  |  sed 's/[:,;!?\.()<>]\+/ & /g;s/  */ /g' | tr '[\t ]' '\n'  | apertium-destxt | hfst-proc -w ../grn.automorf.hfst  | apertium-retxt | grep -v '^$' | wc -l`
-known=`find . -name 'grn_human_rights.txt' | xargs cat  |  sed 's/[:,;!?\.()<>]\+/ & /g;s/  */ /g' | tr '[\t ]' '\n'  | apertium-destxt | hfst-proc -w ../grn.automorf.hfst  | apertium-retxt | grep -v '^$' | grep -v '\*'  | wc -l`
-
-echo "TOTAL: $total KNOWN: $known"
-echo "($known/$total)*100" | bc -l
-
